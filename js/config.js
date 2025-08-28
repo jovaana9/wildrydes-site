fd981c24cb8fcdb681601e8007cae03ba20b5768
Jovana Bošković <116906101+jovaana9@users.noreply.github.com>
2025-08-28 21:20:29 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_0R0BGaw7A', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7pbouums4rcgtl6d8u9fudlbh9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'eu-central-1_Bn2uXkXky', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5vgt9v21fltpe3dcljt92ejunq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
