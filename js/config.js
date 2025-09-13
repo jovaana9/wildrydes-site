6e493a99f4c09b1bff2a03a6ca3ae9226f0e82c4
Jovana Bošković <116906101+jovaana9@users.noreply.github.com>
2025-09-13 15:33:24 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,10 @@ window._config = {
         region: 'us-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://pu6onsw5e4.execute-api.eu-central-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
